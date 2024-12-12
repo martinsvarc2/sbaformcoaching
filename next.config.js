a7233858838f78f3fa82eb1835fde80bb2c83b1e
martinsvarc3 <linkedinautomation2024@gmail.com>
2024-12-13 00:33:20 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,19 +3,6 @@ const nextConfig = {
   images: {
     domains: ['framerusercontent.com', 'res.cloudinary.com'],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://*.framer.com https://*.framer.app"
-          }
-        ],
-      },
-    ]
-  }
 }
 
 module.exports = nextConfig
