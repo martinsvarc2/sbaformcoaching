013615089ea5bf9ec28651344ba310595a4b1c90
avamus <lukassys2004@gmail.com>
2024-12-12 17:51:01 +0100

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import MultiStepForm from './components/multi-step-form'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black py-12">
+      <MultiStepForm />
+    </main>
+  )
+}
+
